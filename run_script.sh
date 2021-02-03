e4205a9453879c74ca24b3abd622acd4d133e034
Lisa Gaedke-Merzhäuser <gaedkem@icslogin01.cluster.net>
2021-02-03 13:32:20 +0100

    integrated main

---
+++ run_script.sh
@@ -0,0 +1,14 @@
+#mpirun -np 2 ./RGFSolver 42 3 data/A_126_126_ns42_nt3.dat
+
+
+ns=$1
+nt=$2
+no=$3
+
+folder_path=/home/gaedkem/spat_temp_m_pardiso/matrices_theta/theta_noise_5/theta_5_-10_2.5_1_ns${ns}_nt${nt}
+folder_path_data=/home/gaedkem/spat_temp_m_pardiso/data/temperature_data_2019/ns${ns}_nt${nt}
+
+nb=2
+
+echo "mpirun -np 1 ./RGFSolver ${folder_path} ${ns} ${nt} ${nb} ${folder_path_data} ${no} >${folder_path}/RGF_output_sel_inv.txt"
+mpirun -np 1 ./RGFSolver ${folder_path} ${ns} ${nt} ${nb} ${folder_path_data} ${no} >${folder_path}/RGF_output_sel_inv.txt
