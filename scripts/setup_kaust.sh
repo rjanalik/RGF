19a0d58eeac0c94f79e0a7e505aada0f3fd32eb8
Georg R. Pollak <pollakg@student.ethz.ch>
2021-06-30 19:07:12 +0300

    Updates setup_kaust.sh

--- scripts/setup_kaust.sh
+++ scripts/setup_kaust.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source /opt/intel/mkl/bin/mklvars.sh intel64
-export LD_LIBRARY_PATH=/home/x_gaedkelb/applications/magma-2.5.4/lib:$LD_LIBRARY_PATH
-export MAGMA_DIR=/home/x_gaedkelb/applications
+source /opt/intel/oneapi/mkl/latest/env/vars.sh  intel64
+export LD_LIBRARY_PATH=~/RGF/external/magma-2.5.4/lib:$LD_LIBRARY_PATH
+export MAGMA_DIR=~/RGF/external
