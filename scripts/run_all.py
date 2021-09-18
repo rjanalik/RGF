9dc22b76d50e2acecab319239f8e2a2ea3728c30
Georg R. Pollak <pollakg@student.ethz.ch>
2021-09-18 18:03:58 +0300

    fixes bug in run_all.py script

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -12,23 +12,27 @@ def get_list_of_experiments(base_path):
     list_of_y_files = list()
     experiments = list()
     for (dirpath, dirnames, filenames) in os.walk(base_path):
-        list_of_y_files += [file for file in filenames if file[0]=="y"]
+        y_files = [file for file in filenames if file.startswith('y_')]
         # path_of_y_files += [os.path.join(dirpath, file) for file in filenames if file[0]=="y"]
-        for file in list_of_y_files:
-            pieces =  file.split("_")
+        if y_files:
+            y_file = y_files[0]
+            pieces =  y_file.split("_")
             no = pieces[1]
             ns = int(re.sub('\D', '', pieces[3]))
             nt = int(re.sub('\D', '', pieces[4]))
-            experiments.append((no, nt, nt))
+            experiments.append((no, ns, nt))
     return experiments
 
 if __name__ == '__main__':
-    if(OVERWRITE_RESULTS):
-        with open('../results/ghcn/results.csv', "w") as results_file:
-            results_file.write('Date/Time,no,ns,nt,t_factorise,flops_factorize,t_solve,flops_solve,t_inv,flops_inv')
-
     base_path = "../data/input/ghcn/2019/spatio_temporal/"
     experiments = get_list_of_experiments(base_path)
     print("Running %d experiments" % len(experiments))
+
+    if(OVERWRITE_RESULTS):
+        with open('../results/ghcn/results.csv', "w") as results_file:
+            results_file.write('Date/Time,no,ns,nt,t_factorise,flops_factorize,t_solve,flops_solve,t_inv,flops_inv\n')
     for no, ns, nt in tqdm(experiments):
-        error = subprocess.run(['./run_single.sh', 'no='+str(no), 'ns='+str(ns), 'nt='+str(nt)], shell=True, capture_output=True)
+        # print("Running no=%s, ns=%s nt=%s  experiments" % (no, ns, nt))
+        result = subprocess.run(['bash ./run_single.sh '+str(no)+' '+str(ns)+' '+str(nt)], shell=True, capture_output=True)
+        # print(result.stdout)
+        # print(result.stderr)
--- scripts/run_single.sh
+++ scripts/run_single.sh
@@ -1,22 +1,32 @@
+#!/bin/bash
 #mpirun -np 2 ./RGFSolver 42 3 data/A_126_126_ns42_nt3.dat
 
 
-: ${ns:=1212}
-: ${nt:=20}
-: ${no:=236680}
-# ns=$1
-# nt=$3
-# no=$3
+no=$1
+ns=$2
+nt=$3
+# environment=${environment:-production}
+# school=${school:-is out}
+
+no=${1:-236680}
+ns=${2:-1212}
+nt=${3:-20}
+nb=${4:-2}
+year=${5:-2019}
+
+while [ $# -gt 0 ]; do
+   if [[ $1 == *"b'--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+        echo $1 $2 "success"
+   fi
+  shift
+done
 
 #folder_path=/home/x_gaedkelb/RGF/data/ns${ns}
-folder_path=/home/x_pollakgr/RGF/data/input/ghcn/2019/spatio_temporal/ns${ns}_nt${nt}
-
-nb=2
-year=2019
+folder_path=/home/x_pollakgr/RGF/data/input/ghcn/${year}/spatio_temporal/ns${ns}_nt${nt}
 
 echo "GV100"
 echo "GPU 0 ./main ${folder_path} ${ns} ${nt} ${nb} ${no} >${folder_path}/RGF_output_sel_inv.txt"
-
-#CUDA_VISIBLE_DEVICES="0" ./mainConstInd ${folder_path} ${ns} ${nt} ${nb} ${folder_path_data} ${no} >${folder_path}/RGF_output.txt
 CUDA_VISIBLE_DEVICES="0" /home/x_pollakgr/RGF/build/bin/main ${folder_path} ${ns} ${nt} ${nb} ${folder_path_data} ${no}
 #mv ${folder_path_data}/log_RGF_ns${ns}_nt${nt}_nb${nb}_no${no}_year${year}.dat ${folder_path_data}/log_RGF_ns${ns}_nt${nt}_nb${nb}_no${no}_year${year}_gpu0.dat
