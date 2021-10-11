5a86354de49a2c31b40fc07faf2d792e6a68a528
Georg R. Pollak <pollakg@student.ethz.ch>
2021-10-12 00:05:49 +0300

    fixes wrong magma lib path

--- scripts/setup_kaust.sh
+++ scripts/setup_kaust.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source /opt/intel/oneapi/mkl/latest/env/vars.sh  intel64
-export LD_LIBRARY_PATH=~/RGF/external/magma-2.5.4/lib:$LD_LIBRARY_PATH
-export MAGMA_DIR=~/RGF/external
+export LD_LIBRARY_PATH=/home/x_pollakgr/RGF/applications/magma-2.5.4/lib:$LD_LIBRARY_PATH
+export MAGMA_DIR=/home/x_pollakgr/RGF/applications
